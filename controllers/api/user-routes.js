62c07197e2b85f7263fc9d48822c15058ae481dc
smandla <35283559+smandla@users.noreply.github.com>
2022-08-01 14:43:46 -0700

    login and logout work

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -15,7 +15,47 @@ router.post("/", async (req, res) => {
       res.status(200).json(dbUserData);
     });
   } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//login
+router.post("/login", async (req, res) => {
+  try {
+    const dbUserData = await User.findOne({ where: { email: req.body.email } });
+    if (!dbUserData) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password. Please try again!" });
+      return;
+    }
+    const validPassword = await dbUserData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password. Please try again!" });
+      return;
+    }
+    req.session.save(() => {
+      req.session.loggedIn = true;
+
+      res
+        .status(200)
+        .json({ user: dbUserData, message: "You are now logged in!" });
+    });
+  } catch (error) {
     res.status(500).json(err);
+  }
+});
+
+//logout
+router.post("/logout", (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
   }
 });
 module.exports = router;
