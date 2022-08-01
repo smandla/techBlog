d336c9b47e9025bafc4ec2057a5abf9593f932b5
smandla <35283559+smandla@users.noreply.github.com>
2022-08-01 11:04:52 -0700

    start server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+
+//To  store the session information in the db, we will use express-session-sequeelize;
+// const SequelizeStore = require("");
