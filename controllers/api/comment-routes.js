06dfacde0b1ac37c4829d2d9c1fc3edd5e99e305
smandla <35283559+smandla@users.noreply.github.com>
2022-08-03 23:07:57 -0700

    fix favicon

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,17 +1,31 @@
 const router = require("express").Router();
 
-const { Blog, Comment, User } = require("../models");
-const withAuth = require("../utils/auth");
+const { Blog, Comment, User } = require("../../models");
+const withAuth = require("../../utils/auth");
 
 router.post("/:id", withAuth, async (req, res) => {
   try {
     const dbCommentData = await Comment.create({
       ...req.body,
-      creator: req.session.creator,
+      user_id: req.session.creator,
+      blog_id: Number(req.params.id),
     });
     res.status(200).json(dbCommentData);
   } catch (error) {
     res.status(400).json(error);
   }
+});
+
+router.put("/update/:id", withAuth, async (req, res) => {
+  try {
+    const commentData = await Comment.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!commentData[0]) {
+      res.status(404).json({});
+    }
+  } catch (error) {}
 });
 module.exports = router;
